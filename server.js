33e0af6ea0f3127ae5c43afcd2654da40b5ceb68
aadarshkumaran <aadarshkumaranak@gmail.com>
2023-05-25 11:47:16 +0530

    heroku disabled

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 const colors = require('colors')
 const morgan = require('morgan')
 const connectDB = require('./config/db')
+const path = require('path')
 const app = new express()
 
 dotenv.config({path:'./config/config.env'})
@@ -18,6 +19,17 @@ if(process.env.NODE_ENV === 'development'){
 
 // app.get('/',(req,res) => res.send('Hello World'))
 app.use('/api/v1/transactions',transactions)
+
+// //serve static assets
+// if(process.env.NODE_ENV === 'production'){
+//     //set static folder
+//     app.use(express.static('client/build'))
+//     app.get('*',(req,res)=>{
+//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+//     })
+// }
+
+// //create post build script
 
 const PORT = process.env.PORT || 5000
 
